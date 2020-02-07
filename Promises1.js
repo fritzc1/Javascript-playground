a3cf39f3d02d949acd6e3bbac2f0d70ac8d96acd
CHRISTOPHER FRITZ <cfritz@us.ibm.com>
2020-02-07 11:38:32 -0500

    fix crappy tabbing

--- Promises1.js
+++ Promises1.js
@@ -7,8 +7,8 @@
  */
 
 var promise = new Promise(function(resolve, reject) { 
-    //reject('Promise Rejected') 
-    resolve('Promise Resolved')
+  //reject('Promise Rejected') 
+  resolve('Promise Resolved')
 }) 
 
 function myPromiseFunc(successMessage) {
@@ -16,24 +16,24 @@ function myPromiseFunc(successMessage) {
   return (successMessage)
 }
 function myPCChange(successMessage) {
-	console.log("then3" + successMessage);
-	return (successMessage)
+  console.log("then3" + successMessage);
+  return (successMessage)
 }
 function myConflictChg(successMessage) {
-	console.log("thenC" + successMessage);
+  console.log("thenC" + successMessage);
 }
 
 
 promise 
-	.then(function(successMessage) { 
+  .then(function(successMessage) { 
 		console.log("then:" + successMessage); 
 		return "pass to next then";
 	})
   .then(myPromiseFunc)
   .then(myPCChange)
-	.then(myConflictChg)
-	.catch(function(errorMessage) { 
-	//error handler function is invoked 
-		console.log("catch:" + errorMessage); 
-    });
+  .then(myConflictChg)
+  .catch(function(errorMessage) { 
+    //error handler function is invoked 
+    console.log("catch:" + errorMessage); 
+  });
 
