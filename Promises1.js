0ce9ee105bee44399d5c9969fc94813d0957a498
CHRISTOPHER FRITZ <cfritz@us.ibm.com>
2020-02-07 11:35:06 -0500

    user update promise

--- Promises1.js
+++ Promises1.js
@@ -12,7 +12,11 @@ var promise = new Promise(function(resolve, reject) {
 }) 
 
 function myPromiseFunc(successMessage) {
-	console.log("then2" + successMessage);
+  console.log("then2" + successMessage);
+  return (successMessage)
+}
+function myPCChange(successMessage) {
+	console.log("then3" + successMessage);
 }
 
 promise 
@@ -20,7 +24,8 @@ promise
 		console.log("then:" + successMessage); 
 		return "pass to next then";
 	})
-	.then(myPromiseFunc)
+  .then(myPromiseFunc)
+  .then(myPCChange)
 	.catch(function(errorMessage) { 
 	//error handler function is invoked 
 		console.log("catch:" + errorMessage); 
