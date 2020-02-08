16f782d0587aba4f012b8c910880822a0dc2b003
fritzc1 <ninhowfritz@gmail.com>
2020-02-08 09:41:49 -0500

    testing callbacks w global var

--- Promises2.js
+++ Promises2.js
@@ -2,5 +2,24 @@ var mypromise = new Promise( (resolve, reject) => {
   resolve("my promise");
 })
 
+var globalVar = 'testing'
+
 mypromise
-  .then(()=>{},(result) => {console.log(result)})
+  .then( // this "then" WOULD like a "catch" if the resolve handler was null
+    (result) => console.log(result + " was resolved woo!"), // resolve handler
+    (result) => console.log(result + " was rejected") // reject handler
+  )
+  .then( // 2nd then.. handler MUST BE A FUNCTION!!!
+    () => {callbackSomething(theCallback)} // call our function and pass callback function in
+  );
+
+function callbackSomething(myCall) {
+  globalVar = globalVar+'1' // modify a global variable from a function
+  myCall('hi');
+}
+
+function theCallback(parm) {
+  globalVar = globalVar+'2' // modify a global variable from the callback function
+  console.log(globalVar);
+}
+
